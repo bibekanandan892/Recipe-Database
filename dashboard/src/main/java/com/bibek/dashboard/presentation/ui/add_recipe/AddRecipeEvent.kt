31c8032c4a1f20a0f28250ce3443f33e122506bf
ISU_561 <bibekananda.n@gmail.com>
2024-07-24 14:30:28 +0530

    Add recipe viewmodel and event and state added.

--- dashboard/src/main/java/com/bibek/dashboard/presentation/ui/add_recipe/AddRecipeEvent.kt
+++ dashboard/src/main/java/com/bibek/dashboard/presentation/ui/add_recipe/AddRecipeEvent.kt
@@ -1,7 +1,5 @@
 package com.bibek.dashboard.presentation.ui.add_recipe
 
-import com.bibek.dashboard.presentation.ui.recipe_details.RecipeDetailsEvent
-
 sealed interface AddRecipeEvent {
     data class GetRecipe(val recipeId: String) :
         AddRecipeEvent
