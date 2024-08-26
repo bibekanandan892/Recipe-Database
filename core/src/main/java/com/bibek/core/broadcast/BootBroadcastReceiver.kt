426de179b9fb812b07b40abcf4a253803548bf09
BIBEKANANDA NAYAK <48781831+bibekanandan892@users.noreply.github.com>
2024-08-26 23:43:23 +0530

    Add boot complete alarm schedule broadcast receiver

---
+++ core/src/main/java/com/bibek/core/broadcast/BootBroadcastReceiver.kt
@@ -0,0 +1,58 @@
+package com.bibek.core.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.bibek.core.data.local.dao.RecipeAlarmDao
+import com.bibek.core.di.AlarmReceiverEntryPoint
+import com.bibek.core.utils.ALARM_ID
+import com.bibek.core.utils.TIME_TO_EAT_MESSAGE
+import com.bibek.core.utils.alarm.scheduleWeeklyAlarm
+import com.bibek.core.utils.notification.showCustomNotification
+import dagger.hilt.android.EntryPointAccessors
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class BootBroadcastReceiver : BroadcastReceiver() {
+    private lateinit var recipeAlarmDao: RecipeAlarmDao
+
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            if (context == null) return
+
+            val hiltEntryPoint =
+                EntryPointAccessors.fromApplication(context, AlarmReceiverEntryPoint::class.java)
+
+            recipeAlarmDao = hiltEntryPoint.recipeAlarmDao()
+
+            val exceptionHandler = hiltEntryPoint.coroutineExceptionHandler()
+            val dispatchers = hiltEntryPoint.coroutinesDispatchers()
+
+            GlobalScope.launch(exceptionHandler + dispatchers.io()) {
+                recipeAlarmDao.getAllRecipeAlarm().first().forEach { recipeAlarmEntity ->
+                    recipeAlarmEntity.apply {
+                        if (isRepeat) {
+                            withContext(dispatchers.main()) {
+                                scheduleWeeklyAlarm(
+                                    context = context,
+                                    alarmId = id ?: return@withContext,
+                                    dayOfWeek = dayOfWeek ?: return@withContext,
+                                    hour = hour ?: return@withContext,
+                                    minute = minute ?: return@withContext,
+                                    isRepeat = true,
+                                    recipeId = recipeId,
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
